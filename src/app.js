c875eac1224498e6c916543668f7e1c6140eb133
LaisLuderer <38754177+LaisLuderer@users.noreply.github.com>
2020-08-19 22:49:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,60 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  //Listar todos os repositórios
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const newRepository = {
+    id: uuid(), //id: "uuid"
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(newRepository);
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repo = repositories.find(repo => repo.id === id);
+  if(!repo){
+    return response.status(400).send();
+  }
+  
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+  
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repo = repositories.findIndex(repo => repo.id === id);
+  if(repo < 0){
+    return response.status(400).send();
+  }
+  repositories.splice(repo, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repo = repositories.find(repo => repo.id === id);
+
+  if(!repo) {
+    return response.status(400).json({ error: 'Repository does not exist.'});
+  }
+  repo.likes++;
+
+  return response.json(repo);
 });
 
 module.exports = app;
